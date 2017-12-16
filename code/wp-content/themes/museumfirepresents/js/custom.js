e24f7696dd893326643a1789d4607e91d4654354
Alban Bailly <alban@zivtech.com>
2017-12-16 21:56:26 +0000

    changes

--- code/wp-content/themes/museumfirepresents/js/custom.js
+++ code/wp-content/themes/museumfirepresents/js/custom.js
@@ -2,3 +2,14 @@
 
 var $ = jQuery;
 
+$('.audiotheme_gig .gig-venue-name').wrap('<a class="mapLink" href="javascript:" \>)');
+
+$('.audiotheme_gig .venue-meta').hide().prepend('<a id="closeMap" href="javascript:">CLOSE</a>');
+
+$('.mapLink').on('click', function() {
+  $('.venue-meta').show();
+});
+
+$('#closeMap').on('click', function() {
+  $('.venue-meta').hide();
+});

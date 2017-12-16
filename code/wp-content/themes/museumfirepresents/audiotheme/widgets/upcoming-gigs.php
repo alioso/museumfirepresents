7a5e93850b9920902257e4a4b3aba09d6f8cf6b3
Alban Bailly <alban@zivtech.com>
2017-12-16 21:08:45 +0000

    changes

---
+++ code/wp-content/themes/museumfirepresents/audiotheme/widgets/upcoming-gigs.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template to display an Upcoming Gigs widget.
+ *
+ * @package AudioTheme\Template
+ * @since 1.5.0
+ */
+?>
+
+<?php
+if ( ! empty( $title ) ) :
+	echo $before_title . $title . $after_title;
+endif;
+?>
+
+<?php if ( $loop->have_posts() ) : ?>
+
+	<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+		<dl class="vevent">
+
+			<?php
+			$gig = get_audiotheme_gig();
+			echo get_audiotheme_gig_link( $gig, array( 'before' => '<dt>', 'after' => '</dt>', 'microdata' => false ) );
+			?>
+
+            <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail( $page->ID, 'full' ); ?></a>
+            <br /><br />
+			<?php if ( audiotheme_gig_has_venue() ) : ?>
+				<dd class="location">
+					<span class="gig-title"><?php echo get_audiotheme_gig_location(); ?></span>
+				</dd>
+			<?php endif; ?>
+
+			<dd class="date">
+				<meta content="<?php echo esc_attr( get_audiotheme_gig_time( 'c' ) ); ?>">
+				<time class="dtstart" datetime="<?php echo esc_attr( get_audiotheme_gig_time( 'c' ) ); ?>">
+					<?php echo get_audiotheme_gig_time( $date_format ); ?>
+				</time>
+			</dd>
+
+			<?php if ( ! empty( $gig->post_title ) && audiotheme_gig_has_venue() ) : ?>
+				<dd class="venue"><?php echo esc_html( get_audiotheme_venue( $gig->venue->ID )->name ); ?></dd>
+			<?php endif; ?>
+
+			<?php if ( $gig_description = get_audiotheme_gig_description() ) : ?>
+				<dd class="description"><?php echo wp_strip_all_tags( $gig_description ); ?></dd>
+			<?php endif; ?>
+
+		</dl>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>

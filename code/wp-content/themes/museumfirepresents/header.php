4165f5d32a854fc8ec0403905db3dd6a7ce1c128
Alban Bailly <alban@zivtech.com>
2018-02-05 00:23:55 -0500

    eagles

---
+++ code/wp-content/themes/museumfirepresents/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the theme header.
+ *
+ * @package Marquee
+ * @since 1.0.0
+ */
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<?php
+$page = str_replace(array('/', '.php', '?pg='), '', $_SERVER['REQUEST_URI']);
+$page = $page ? $page : 'default';
+
+/* Sanitize $page! */
+
+?>
+
+<body id="<?php echo $page ?>" <?php body_class(); ?> <?php marquee_body_schema(); ?>>
+
+	<?php do_action( 'marquee_before' ); ?>
+
+	<div id="viewport-pane">
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'marquee' ); ?></a>
+
+			<?php do_action( 'marquee_header_before' ); ?>
+
+			<?php get_template_part( 'templates/parts/site-header' ); ?>
+
+			<?php do_action( 'marquee_header_after' ); ?>
+
+			<div id="content" class="site-content"><!--pjax-content-->
+
+				<?php do_action( 'marquee_main_before' ); ?>
